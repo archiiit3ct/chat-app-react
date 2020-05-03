96308dcea69df31399d4e6cd2003d0dc30e968b8
Tim <terrykane@protonmail.com>
2020-05-04 00:51:16 +0300

    Fixed

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -9,14 +9,14 @@ const TextContainer = ({ users }) => (
   <div className="textContainer">
     {
       users
-        ? ( // Check are there any users
+        ? (
           <div>
-            <h1>Users in a room:</h1>
+            <h1>People in room:</h1>
             <div className="activeContainer">
               <h2>
-                {users.map(({name}) => ( // Display users
+                {users.map(({name}) => (
                   <div key={name} className="activeItem">
-                    {name[0].toUpperCase() + name.slice(1)}
+                    {name}
                     <img alt="Online Icon" src={onlineIcon}/>
                   </div>
                 ))}
